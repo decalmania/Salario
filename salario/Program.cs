7a7972834ee9ac1fcebd8b0f06948941f39d98c5
Usuario <Usuario@INVENT081>
2023-10-30 10:48:16 -0300

    Adicionar arquivos de projeto.

---
+++ salario/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string nome = (Console.ReadLine());
+        double salario = double.Parse(Console.ReadLine());
+        double totalVenda = double.Parse(Console.ReadLine());
+        double total = (0.15 * totalVenda) + salario;
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2"));
+    }
+
+}
